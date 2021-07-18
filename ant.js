225a9f3f9f140d8b3686c7c954566adb283585e1
Greg K <gkreisman_cep@qq.com>
2021-07-19 07:58:02 +0800

    ant class working for one ant

--- ant.js
+++ ant.js
@@ -1,7 +1,80 @@
 // ant class
+// will not have a show function as the world is shown
 
 let ANTUP =0;
 let ANTRIGHT =1;
 let ANTDOWN =2;
 let ANTLEFT =3;
 
+class Ant{
+    constructor(x,y,clr){
+        this.x = x;
+        this.y =y;
+        this.clr =clr; // color is either 1 or 2 as it is marks which ant in the ant world
+        this.dir = 0;
+        this.sz = 10;
+    }
+
+    turnRight(){
+        this.dir++; 
+         if (this.dir > ANTLEFT){
+           this.dir = ANTUP;
+         }
+       }
+       
+       
+    turnLeft(){
+        this.dir--;
+         if (this.dir < ANTUP){
+           this.dir = ANTLEFT;
+         }
+       }
+       
+    moveForward(){
+         if (this.dir == ANTUP){
+          this.y--; 
+         }else if (this.dir == ANTDOWN){
+          this.y++; 
+           
+         }else if (this.dir == ANTRIGHT){
+          this.x++; 
+           
+         }else if (this.dir == ANTLEFT){
+          this.x--; 
+         }
+         // check edges
+         
+         if (this.x > 80-1) {
+          this.x =0; 
+         } else if (this.x < 0){
+           this.x = 80-1;
+         }
+         
+         if (this.y > 80-1) {
+          this.y =0; 
+         } else if (this.y < 0){
+           this.y = 80-1;
+         }
+         
+       }
+       
+    moveAnt(){
+        let state = antworld[this.x][this.y];
+  
+        if (state == 0){
+            this.turnRight();
+            antworld[this.x][this.y] = this.clr; // places ant symbol
+            this.moveForward(); 
+        }else if (state == 1){
+            this.turnLeft();
+            antworld[this.x][this.y] = 0;
+            this.moveForward();
+        }
+            }
+
+
+
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -9,14 +9,8 @@ let sz =10
 let col =80;
 let row =80;
 let antworld;
-let x, y;   //for ant
-
-let ANTUP =0;
-let ANTRIGHT =1;
-let ANTDOWN =2;
-let ANTLEFT =3;
-
-let dir=0;
+let gregAnt;
+ 
 
 function setup() {
   createCanvas(800, 800);
@@ -24,8 +18,7 @@ function setup() {
   stroke(0);
   
   antworld = twoDee(col,row,0);
-  x = 40;
-  y =40;
+  gregAnt = new Ant(40,40,1)
   
 
   showWorld();
@@ -51,66 +44,13 @@ function twoDee(col, row,data){
   
 } 
 
-function turnRight(){
- dir++; 
-  if (dir > ANTLEFT){
-    dir = ANTUP;
-  }
-}
-
-
-function turnLeft(){
- dir--;
-  if (dir < ANTUP){
-    dir = ANTLEFT;
-  }
-}
-
-function moveForward(){
-  if (dir == ANTUP){
-   y--; 
-  }else if (dir == ANTDOWN){
-   y++; 
-    
-  }else if (dir == ANTRIGHT){
-   x++; 
-    
-  }else if (dir == ANTLEFT){
-   x--; 
-  }
-  // check edges
-  
-  if (x > 80-1) {
-   x =0; 
-  } else if (x < 0){
-    x = 80-1;
-  }
-  
-  if (y > 80-1) {
-   y =0; 
-  } else if (y < 0){
-    y = 80-1;
-  }
-  
-}
 
 function draw(){
   
-  print(x,y);
   for (let n =0; n< 50; n++){
   
+  gregAnt.moveAnt()
   
-  let state = antworld[x][y];
-  
-  if (state == 0){
-    turnRight();
-    antworld[x][y] = 1;
-    moveForward(); 
-  }else if (state == 1){
-    turnLeft();
-    antworld[x][y] = 0;
-    moveForward();
-   }
     
     
  }
